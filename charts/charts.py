9def93fadb90427eefdd4a28e686ae7b4af992ae
asneha0901 <agarwalsneha@uchicago.edu>
2026-03-02 23:39:11 -0600

    fixed more bugs

--- charts/charts.py
+++ charts/charts.py
@@ -1,26 +1,35 @@
 import altair as alt
 import pandas as pd
+from utils.io import chicago_wards_df, cats_sorted, costs_wide
 
-def Intro_chart(chicago_wards_df: pd.DataFrame, cats_sorted, costs_wide: pd.DataFrame):
-    cat_radio = alt.binding_radio(options=cats_sorted, name="Category: ")
-    cat_sel = alt.param(name="cat_sel", value=cats_sorted[0], bind=cat_radio)
 
-    joint_chart = (
-        alt.Chart(chicago_wards_df, title="Expenditure by Ward (select category)")
-        .mark_geoshape(stroke="#706545")
-        .project(type="mercator")
-        .add_params(cat_sel)
-        .transform_lookup(
-            lookup="ward",
-            from_=alt.LookupData(costs_wide, key="ward", fields=list(cats_sorted))
-        ).transform_calculate(selected_cost="datum[cat_sel]")
-        .encode(
-            color=alt.Color("selected_cost:Q", scale=alt.Scale(scheme="greens"), title="Total cost"),
-            tooltip=[
-                alt.Tooltip("ward:O", title="Ward"),
-                alt.Tooltip("selected_cost:Q", title="Total cost", format=",")
-            ],
+cat_radio = alt.binding_radio(options=cats_sorted, name="Category: ")
+cat_sel = alt.param(name="cat_sel", value=cats_sorted[0], bind=cat_radio)
+
+chicago_wards = alt.topo_feature(
+"https://raw.githubusercontent.com/asneha0901/data227_content/refs/heads/main/chicago-ward-boundaries.topojson",
+feature="data"
+)
+spend_by_type = (
+    alt.Chart(chicago_wards)
+    .mark_geoshape(stroke="#706545")
+    .project(type="mercator")
+    .add_params(cat_sel)
+    .transform_lookup(
+        lookup="properties.ward",
+        from_=alt.LookupData(
+            costs_wide,
+            key="ward",
+            fields=list(cats_sorted)  
         )
     )
-
-    return joint_chart
+    .transform_calculate(selected_cost="datum[cat_sel] || 0")
+    .encode(
+        color=alt.Color("selected_cost:Q", scale=alt.Scale(scheme="greens"), title="Total cost").legend(
+            orient="left", title=' ', padding=50),
+        tooltip=[
+            alt.Tooltip("ward:O", title="Ward"),
+            alt.Tooltip("selected_cost:Q", title="Total cost", format=",")
+        ],
+    )
+)
